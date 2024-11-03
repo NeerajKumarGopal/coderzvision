4a00e04a3678f9a157f65156af02a93ac7f801df
NeerajKumarGopal <66553972+NeerajKumarGopal@users.noreply.github.com>
2024-11-03 20:50:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "lamp_server" {
   ami           = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
   key_name      = "coderzvision"
   vpc_security_group_ids = [aws_security_group.lamp_sg.id]
   
@@ -36,6 +37,36 @@ resource "aws_instance" "lamp_server" {
     }
   }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+resource "aws_route_table_association" "my_route_table_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+  
   
   
   user_data = <<-EOF
@@ -57,7 +88,9 @@ resource "aws_instance" "lamp_server" {
 }
 
 resource "aws_security_group" "lamp_sg" {
-  name = "lamp_security_group"
+  name        = "lamp_security_group"
+  description = "security group"
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 80
@@ -80,3 +113,4 @@ resource "aws_security_group" "lamp_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
